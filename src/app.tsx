0a9d9a897a2765375e108cfc41e21742c280f04f
Kirill Orlov <kvorlov@intas.pro>
2025-07-27 21:38:46 +0500

    fix(app): change main page content

--- src/app.tsx
+++ src/app.tsx
@@ -1,46 +1,41 @@
 import './app.css';
 
 function App() {
-  const handleOpen = () => {
-    window.open('https://career.habr.com/orlov_kirill', '_blank', 'noopener,noreferrer');
-  };
+	const handleOpen = () => {
+		window.open('https://career.habr.com/orlov_kirill', '_blank', 'noopener,noreferrer');
+	};
 
-  const currentYear = new Date().getFullYear();
+	const currentYear = new Date().getFullYear();
 
-  return (
-    <main className="app-container">
-      <header className="header">
-        <h1>Кирилл Орлов</h1>
-        <p className="subtitle">
-          Инженер-программист с {currentYear - 2021} годами опыта, специализирующийся на
-          Frontend-разработке
-        </p>
-        <button onClick={handleOpen} className="download-button" aria-label="Скачать резюме">
-          Смотреть резюме
-        </button>
-      </header>
+	return (
+		<main className="app-container">
+			<header className="header">
+				<h1>Кирилл Орлов</h1>
+				<p className="subtitle">
+					Frontend-разработчик с {currentYear - 2021}-х летним опытом создания сложных продуктовых
+					веб-приложений.
+				</p>
+				<button onClick={handleOpen} className="download-button" aria-label="Подробнее обо мне">
+					Подробнее обо мне
+				</button>
+			</header>
 
-      <section className="social-links">
-        <ul className="links-list">
-          <li>
-            <a href="https://github.com/kirill-orlov" target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
-          </li>
-          <li>
-            <a href="https://t.me/orlovkirill" target="_blank" rel="noopener noreferrer">
-              Telegram
-            </a>
-          </li>
-          <li>
-            <a href="mailto:kirillorlovufa@gmail.com" target="_blank" rel="noopener noreferrer">
-              kirillorlovufa@gmail.com
-            </a>
-          </li>
-        </ul>
-      </section>
-    </main>
-  );
+			<section className="social-links">
+				<ul className="links-list">
+					<li>
+						<a href="https://t.me/orlovkirill" target="_blank" rel="noopener noreferrer">
+							Telegram
+						</a>
+					</li>
+					<li>
+						<a href="mailto:kirillorlovufa@gmail.com" target="_blank" rel="noopener noreferrer">
+							kirillorlovufa@gmail.com
+						</a>
+					</li>
+				</ul>
+			</section>
+		</main>
+	);
 }
 
 export default App;
